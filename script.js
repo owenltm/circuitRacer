4437c4f8214d112db95d29188134fc8786d9ea0c
owenltm <owenlwiantoro01@gmail.com>
2024-01-24 22:47:12 +0700

    following mouse pointer

--- script.js
+++ script.js
@@ -1,10 +1,73 @@
+let vehicle;
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+
+  vehicle = new Vehicle(100, 100);
 }
 
+
 function draw() {
-  background(220);
+  background("#212121");
+
   target = createVector(mouseX, mouseY);
-  fill("#212121");
+  fill("#FFF");
   circle(target.x, target.y, 32);
+
+  vehicle.seek(target);
+  vehicle.applyForce();
+  vehicle.update();
+  vehicle.show()
+}
+
+class Vehicle {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.maxSpeed = 4;
+    this.maxForce = 0.25;
+    this.r = 16;
+  }
+
+  seek(target) {
+    let force = p5.Vector.sub(target, this.pos);
+    force.setMag(this.maxSpeed);
+    force.sub(this.vel);
+    force.limit(this.maxForce);
+    this.applyForce(force);
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  show() {
+    fill(255);
+    push()
+    translate(this.pos.x, this.pos.y);
+    rotate(this.vel.heading());
+    rect(-20, -10, 40, 20);
+    pop();
+  }
+
+  edges() {
+    if (this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+    } else if (this.pos.x < -this.r) {
+      this.pos.x = width + this.r;
+    }
+    if (this.pos.y > height + this.r) {
+      this.pos.y = -this.r;
+    } else if (this.pos.y < -this.r) {
+      this.pos.y = height + this.r;
+    }
+  }
 }
